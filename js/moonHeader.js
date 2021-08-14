cf41d4f15b7229781a434aebb7c7fed7dbb6baa3
Arthur Uezu <uezuarthur@gmail.com>
2021-08-14 11:51:02 -0300

    navigation bar and moon animation

---
+++ js/moonHeader.js
@@ -0,0 +1,11 @@
+let moon = document.getElementById("moon");
+var time = 0;
+moon.style.display=
+setInterval(function(){
+    time++;
+    moon.style.top= 45-Math.sqrt(time)+"px";
+    moon.style.left =((window.innerWidth/2)-200+time+"px");
+    if(time==340){
+        time = 0;
+    }
+},400);

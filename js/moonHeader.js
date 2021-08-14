51a004b388b5cc05dc06cdd7c02e552cfd92e331
Arthur Uezu <uezuarthur@gmail.com>
2021-08-14 14:59:18 -0300

    started mediaquerys

--- js/moonHeader.js
+++ js/moonHeader.js
@@ -1,9 +1,7 @@
 let moon = document.getElementById("moon");
 var time = 0;
-moon.style.display=
 setInterval(function(){
     time++;
-    moon.style.top= 46+"px";
     moon.style.left =((window.innerWidth/2)-200+time+"px");
     if(time==340){
         time = 0;
